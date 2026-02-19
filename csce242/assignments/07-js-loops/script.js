2677766d930c71ef01818523a56bb53c1a9ab21f
Grace-Cina <Grace.cina9@gmail.com>
2026-02-18 20:34:41 -0500

    a few fisxes to the fish bowl

--- csce242/assignments/07-js-loops/script.js
+++ csce242/assignments/07-js-loops/script.js
@@ -1,30 +1,27 @@
 const water = document.getElementById("water");
 
-const random = (min,max) => Math.random()*(max-min)+min;
+const rand = (min,max)=>Math.random()*(max-min)+min;
 
-const createBubble = () => {
-  const b = document.createElement("div");
-  b.className = "bubble";
+const makeBubble = ()=>{
+  const b=document.createElement("div");
+  b.className="bubble";
 
-  const size = random(6,14);
-  b.style.width = size+"px";
-  b.style.height = size+"px";
+  const size=rand(6,14);
+  b.style.width=size+"px";
+  b.style.height=size+"px";
 
-  b.style.left = random(0, water.clientWidth-size)+"px";
+  b.style.left=rand(0,water.clientWidth-size)+"px";
 
-  const speed = random(3,6);
-  b.style.animationDuration = speed+"s";
-
-  const delay = random(0,3);
-  b.style.animationDelay = delay+"s";
+  b.style.animationDuration=rand(3,6)+"s";
+  b.style.animationDelay=rand(0,3)+"s";
 
   water.appendChild(b);
 };
 
-const makeBubbles = () => {
-  for(let i=0;i<18;i++){   // REQUIRED FOR LOOP
-    createBubble();
+const buildBubbles=()=>{
+  for(let i=0;i<20;i++){   // REQUIRED FOR LOOP
+    makeBubble();
   }
 };
 
-window.onload = makeBubbles;
+window.onload=buildBubbles;
